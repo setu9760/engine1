da0e9ab926ff8afb01a964eaa6567a9a2b044975
Setu Desai <setu.desai@smartstream-stp.com>
2018-10-01 16:30:04 +0100

    remove spring packaging and fallback to traditional war package.

--- engine1-war/src/main/java/com/mycompany/engine1/Engine1Application.java
+++ engine1-war/src/main/java/com/mycompany/engine1/Engine1Application.java
@@ -2,11 +2,18 @@ package com.mycompany.engine1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Engine1Application {
+public class Engine1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Engine1Application.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Engine1Application.class);
+    }
 }
